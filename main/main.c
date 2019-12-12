5227f1d4a50e0889f4026d96b42699c5e8d7d486
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2019-12-12 14:25:39 +0200

    Release mqtt brightness and power

--- main/main.c
+++ main/main.c
@@ -1,10 +1,13 @@
 #include <string.h>
+#include <stdbool.h>
 #include "sdkconfig.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include <cJSON.h>
+#include <driver/gpio.h>
+#include <driver/ledc.h>
 
 #include "esp_system.h"
 #include "esp_spi_flash.h"
@@ -32,47 +35,73 @@ const char *MQTT_SET_TOPIC = "discovery/light/esp32_yellow_garland/set";
 const char *MQTT_STATE_TOPIC = "discovery/light/esp32_yellow_garland/state";
 
 bool DEFAULT_STATE_POWER = true;
-uint8_t DEFAULT_STATE_BRIGHTNESS = 100;
+int DEFAULT_STATE_BRIGHTNESS = 255;
 
 #define WIFI_MAXIMUM_RETRY 15
 #define DEFAULT_BUF_SIZE 1024
+#define GPIO_OUTPUT_IO 19
+#define GPIO_OUTPUT_PIN_SEL  1ULL<<GPIO_OUTPUT_IO
+
 static int wifi_retry_num = 0;
 
-typedef struct {
-    bool power;
-    uint8_t brightness;
-    uint8_t *effect;
+typedef struct esp_state_t {
+    bool *power;
+    int *brightness;
+    int *effect;
 } *esp_state;
 
-static esp_state current_state;
+esp_state current_state;
 
-void build_esp_state(esp_state state) {
-    state->power = DEFAULT_STATE_POWER;
-    state->brightness = DEFAULT_STATE_BRIGHTNESS;
+ledc_timer_config_t ledc_timer = {
+        .duty_resolution = LEDC_TIMER_8_BIT, // resolution of PWM duty
+        .freq_hz = 5000,                      // frequency of PWM signal
+        .speed_mode = LEDC_HIGH_SPEED_MODE,           // timer mode
+        .timer_num = LEDC_TIMER_0,            // timer index
+        .clk_cfg = LEDC_AUTO_CLK,              // Auto select the source clock
+};
+
+ledc_channel_config_t ledc_channel = {
+        .channel    = LEDC_CHANNEL_0,
+        .duty       = 0,
+        .gpio_num   = GPIO_OUTPUT_IO,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .hpoint     = 0,
+        .timer_sel  = LEDC_TIMER_0,
+        .intr_type  = LEDC_INTR_FADE_END
+};
+
+esp_state build_esp_state() {
+    esp_state state = malloc(sizeof(struct esp_state_t));
+    state->power = NULL;
+    state->brightness = NULL;
     state->effect = NULL;
+
+    return state;
 }
 
 char * serialize_esp_state(esp_state state) {
     char *json = NULL;
     cJSON *root = cJSON_CreateObject();
-
-    cJSON_AddStringToObject(root, "uniq_id", "ffffffffffffffff");
+//    cJSON_AddStringToObject(root, "uniq_id", "ffffffffffffffff");
 
     if (state->power) {
-        cJSON_AddStringToObject(root, "state", STATE_POWER_ON);
-    } else {
-        cJSON_AddStringToObject(root, "state", STATE_POWER_OFF);
+        if (*state->power) {
+            cJSON_AddStringToObject(root, "state", STATE_POWER_ON);
+        } else {
+            cJSON_AddStringToObject(root, "state", STATE_POWER_OFF);
+            goto end;
+        }
     }
 
-    cJSON_AddNumberToObject(root, "brightness", state->brightness);
+    if (state->brightness) {
+        cJSON_AddNumberToObject(root, "brightness", *state->brightness);
+    }
 
-    // TODO: use the map to cast int to string
     if (state->effect) {
         cJSON_AddNumberToObject(root, "effect", *state->effect);
-    } else {
-        cJSON_AddNullToObject(root, "effect");
     }
 
+    end:
     json = cJSON_PrintUnformatted(root);
     cJSON_Delete(root);
     return json;
@@ -82,18 +111,28 @@ void deserialize_esp_state(esp_state state, char *json) {
     cJSON *root = cJSON_Parse(json);
 
     cJSON *power = cJSON_GetObjectItem(root, "state");
-
     if (power) {
-        state->power = strcmp(power->valuestring, STATE_POWER_ON) == 0;
+        if (!state->power) {
+            state->power = (bool*)malloc(sizeof(bool));
+        }
+        *state->power = strcmp(power->valuestring, STATE_POWER_ON) == 0;
     }
 
     cJSON *brightness = cJSON_GetObjectItem(root, "brightness");
-
     if (brightness) {
-        state->brightness = brightness->valueint;
+        if (!state->brightness) {
+            state->brightness = (int*)malloc(sizeof(int));
+        }
+        *state->brightness = brightness->valueint;
     }
 
-    state->effect = NULL;
+    cJSON *effect = cJSON_GetObjectItem(root, "effect");
+    if (effect) {
+        if (!state->effect) {
+            state->effect = (int*)malloc(sizeof(int));
+        }
+        *state->effect = effect->valueint;
+    }
 
     cJSON_Delete(root);
 }
@@ -102,42 +141,53 @@ bool has_esp_state_file() {
     return true;
 }
 
-void save_esp_state(esp_state state) {
-//    char *json = serialize_esp_state(state);
-}
+//void save_esp_state(esp_state state) {
+////    char *json = serialize_esp_state(state);
+//}
 
-int load_esp_state(esp_state state) {
+esp_err_t load_esp_state(esp_state state) {
     bool has = has_esp_state_file();
+
     if (has) {
-        char *json = "{\"brightness\":50}";
+        char *json = "{\"power\":\"ON\",\"brightness\":255}";
         deserialize_esp_state(state, json);
+    } else {
+        bool *power = (bool *) malloc(sizeof(int));
+        state->power = power;
+        *state->power = true;
+
+        int *brightness = (int *) malloc(sizeof(int));
+        state->brightness = brightness;
+        *state->brightness = 255;
     }
 
-    return 0;
+    return ESP_OK;
 }
 
-void merge_esp_state(esp_state destination, esp_state source) {
-    destination->power = source->power;
-    destination->brightness = source->brightness;
-    destination->effect = source->effect;
-}
-
-int apply_esp_state(esp_state state, esp_state applied) {
-    if (applied) {
-        merge_esp_state(applied, state);
+esp_err_t apply_esp_state(esp_state state) {
+    if (state->power) {
+        if (!*state->power) {
+            ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, 0);
+            ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
+            return ESP_OK;
+        }
     }
 
-    return EXIT_SUCCESS;
+    if (state->brightness) {
+        ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, *state->brightness);
+        ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
+
+//        ledc_set_fade_with_time(ledc_channel.speed_mode, ledc_channel.channel, *state->brightness, 3000);
+//        ledc_fade_start(ledc_channel.speed_mode, ledc_channel.channel, LEDC_FADE_NO_WAIT);
+//        vTaskDelay(3000 / portTICK_PERIOD_MS);
+    }
+
+    return ESP_OK;
 }
 
-int handle_esp_event(char *data) {
-    esp_state state = malloc(sizeof(esp_state));
-    esp_state applied = malloc(sizeof(esp_state));
-
+int handle_esp_event(esp_state state, char *data) {
     deserialize_esp_state(state, data);
-    apply_esp_state(state, applied);
-    merge_esp_state(current_state, applied);
-
+    apply_esp_state(state);
     return EXIT_SUCCESS;
 }
 
@@ -158,14 +208,13 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
         case MQTT_EVENT_DATA:
             memset(topic, 0, DEFAULT_BUF_SIZE);
             strncpy(topic, event->topic, event->topic_len);
-
             memset(data, 0, DEFAULT_BUF_SIZE);
             strncpy(data, event->data, event->data_len);
 
             ESP_LOGI(TAG, "[MQTT] Data %s with %s", topic, data);
 
             if (strcmp(topic, MQTT_SET_TOPIC) == 0) {
-                handle_esp_event(data);
+                handle_esp_event(current_state, data);
                 char *json = serialize_esp_state(current_state);
                 ESP_LOGI(TAG, "[MQTT] Publish data %.*s", strlen(json), json);
                 esp_mqtt_client_publish(client, MQTT_STATE_TOPIC, json, 0, 2, 0);
@@ -258,7 +307,6 @@ void wifi_init_sta() {
     ESP_ERROR_CHECK(esp_wifi_start());
 }
 
-
 void app_main()
 {
     ESP_LOGI(TAG, "[APP] Startup..");
@@ -275,26 +323,46 @@ void app_main()
     esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
     esp_log_level_set(TAG, ESP_LOG_VERBOSE);
 
-    current_state = malloc(sizeof(esp_state));
-
-    build_esp_state(current_state);
-
-    int error = load_esp_state(current_state);
-    if (error != 0) {
-        ESP_LOGI(TAG, "[APP] Load state error %d", error);
-    }
-
-    apply_esp_state(current_state, NULL);
-
-    char *printed_state = serialize_esp_state(current_state);
-    ESP_LOGI(TAG, "[APP] Current state: %s", printed_state);
-
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
 
+    ledc_timer_config(&ledc_timer);
+    ledc_channel_config(&ledc_channel);
+    ledc_fade_func_install(0);
+
+    current_state = build_esp_state();
+    esp_err_t error = load_esp_state(current_state);
+
+    if (error != ESP_OK) {
+        ESP_LOGI(TAG, "[APP] Load state error %d", error);
+    }
+
+    apply_esp_state(current_state);
+
+    // TODO: use function to dump data instead of serialize
+    char *printed_state = serialize_esp_state(current_state);
+    ESP_LOGI(TAG, "[APP] Current state: %s", printed_state);
+
+//    gpio_config_t io_conf;
+//    //disable interrupt
+//    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+//    //set as output mode
+//    io_conf.mode = GPIO_MODE_OUTPUT;
+//    //bit mask of the pins that you want to set,e.g.GPIO18/19
+//    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+//    //disable pull-down mode
+//    io_conf.pull_down_en = 0;
+//    //disable pull-up mode
+//    io_conf.pull_up_en = 0;
+//    //configure GPIO with the given settings
+//    gpio_config(&io_conf);
+//    gpio_set_level(GPIO_OUTPUT_IO, true);
+
     wifi_init_sta();
-
     xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
-
     mqtt_app_start();
+
+    while (1) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
